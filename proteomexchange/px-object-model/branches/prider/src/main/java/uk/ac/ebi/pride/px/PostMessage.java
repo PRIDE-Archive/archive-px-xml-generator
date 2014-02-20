61c87ae41b4a7dce9be02c35e9662ca43c538c17
tobias <tobias@84e480dd-4284-0410-bcb6-dc83b1e7cf5b>
2014-02-20 11:29:35 +0000

    PostMessage now throws IOException.

--- proteomexchange/px-object-model/branches/prider/src/main/java/uk/ac/ebi/pride/px/PostMessage.java
+++ proteomexchange/px-object-model/branches/prider/src/main/java/uk/ac/ebi/pride/px/PostMessage.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.pride.px;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.HttpMultipartMode;
@@ -13,9 +14,8 @@ import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.ac.ebi.pride.px.xml.XMLParams;
-
-
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
 
 /**
@@ -32,39 +32,34 @@ public class PostMessage {
     //http://central.proteomexchange.org/cgi/GetDataset?ID=PXD000001
     public static final String CHARSET = "UTF-8";
 
-    public static String postMessage(File file, XMLParams params) {
+    public static String postMessage(File file, XMLParams params)throws IOException {
         String serverResponse;
-        try {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost(URL);
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(URL);
 
-            FileBody bin = new FileBody(file);
-            MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE, null, Charset.forName(CHARSET));
+        FileBody bin = new FileBody(file);
+        MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE, null, Charset.forName(CHARSET));
 
-            reqEntity.addPart("ProteomeXchangeXML", bin);
-            reqEntity.addPart("PXPartner", new StringBody(params.getPxPartner(), "text/plain", Charset.forName(CHARSET)));
-            reqEntity.addPart("authentication", new StringBody(params.getAuthentication(), "text/plain", Charset.forName(CHARSET)));
-            reqEntity.addPart("method", new StringBody(params.getMethod(), "text/plain", Charset.forName(CHARSET)));
-            reqEntity.addPart("test", new StringBody(params.getTest(), "text/plain", Charset.forName(CHARSET)));
-            reqEntity.addPart("verbose", new StringBody(params.getVerbose(), "text/plain", Charset.forName(CHARSET)));
+        reqEntity.addPart("ProteomeXchangeXML", bin);
+        reqEntity.addPart("PXPartner", new StringBody(params.getPxPartner(), "text/plain", Charset.forName(CHARSET)));
+        reqEntity.addPart("authentication", new StringBody(params.getAuthentication(), "text/plain", Charset.forName(CHARSET)));
+        reqEntity.addPart("method", new StringBody(params.getMethod(), "text/plain", Charset.forName(CHARSET)));
+        reqEntity.addPart("test", new StringBody(params.getTest(), "text/plain", Charset.forName(CHARSET)));
+        reqEntity.addPart("verbose", new StringBody(params.getVerbose(), "text/plain", Charset.forName(CHARSET)));
 
-            httppost.setEntity(reqEntity);
+        httppost.setEntity(reqEntity);
 
-            // Execute HTTP Post Request
+        // Execute HTTP Post Request
 
-            // ResponseHandler<String> responseHandler=new BasicResponseHandler();
-            HttpResponse response = httpclient.execute(httppost);
-            int statusCode = response.getStatusLine().getStatusCode();
-            HttpEntity entity = response.getEntity();
-            serverResponse = EntityUtils.toString(entity);
-            if (statusCode != 200) {
-                // responseBody will have the error response
-                logger.error("Error from server: " + statusCode + serverResponse);
-            }
-            return serverResponse;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+        // ResponseHandler<String> responseHandler=new BasicResponseHandler();
+        HttpResponse response = httpclient.execute(httppost);
+        int statusCode = response.getStatusLine().getStatusCode();
+        HttpEntity entity = response.getEntity();
+        serverResponse = EntityUtils.toString(entity);
+        if (statusCode != 200) {
+            // responseBody will have the error response
+            logger.error("Error from server: " + statusCode + serverResponse);
         }
+        return serverResponse;
     }
 }
