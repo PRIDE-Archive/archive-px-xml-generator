37f04021b19ec2ad2a16878215b81b99bf7dec41
Tobias-Ternent <tobias@ebi.ac.uk>
2016-07-25 09:39:21 +0100

    Added JavaDoc.

--- src/main/java/uk/ac/ebi/pride/archive/px/Reader/ReadMessage.java
+++ src/main/java/uk/ac/ebi/pride/archive/px/Reader/ReadMessage.java
@@ -15,11 +15,10 @@ public class ReadMessage {
 
     /**
      * Method to read in an existing PX XML file, by unmarshalling it to a ProteomeXchangeDataset.
-     *
      * @param pxFile the PX XML file to read in.
      * @return a ProteomeXchangeDataset ready for changes to then be marshalled into a PX XML file.
+     * @throws IOException
      */
-
     public static ProteomeXchangeDataset readPxXml(File pxFile) throws IOException {
         return new PxUnmarshaller().unmarshall(pxFile);
     }
