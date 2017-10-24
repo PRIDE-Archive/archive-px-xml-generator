7675f4c538bbb447336db8c69b62a35e59748868
Tobias-Ternent <tobias@ebi.ac.uk>
2017-10-24 10:18:25 +0100

    Update schema location, removed whitespace, unused imports.

--- src/main/java/uk/ac/ebi/pride/archive/px/ValidateMessage.java
+++ src/main/java/uk/ac/ebi/pride/archive/px/ValidateMessage.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.pride.archive.px;
 
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
@@ -12,7 +11,6 @@ import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.List;
 
 /**
@@ -23,33 +21,33 @@ import java.util.List;
 
 public class ValidateMessage {
 
-    private static final Logger logger = LoggerFactory.getLogger(ValidateMessage.class);
-    private static final String SCHEMA_LOCATION = "http://proteomecentral.proteomexchange.org/schemas/proteomeXchange-1.3.0.xsd";
+  private static final Logger logger = LoggerFactory.getLogger(ValidateMessage.class);
+  private static final String SCHEMA_LOCATION = "http://ftp.pride.ebi.ac.uk/pride/resources/schema/proteomexchange/proteomeXchange-1.3.0.xsd";
 
-    /**
-     * Method to validate a supplied PX XML file.
-     *
-     * @param file the PX XML file to be validated.
-     * @return String which lists any errors that occurred during validation. If there were none, the String will be empty.
-     * @throws SAXException
-     * @throws MalformedURLException
-     * @throws FileNotFoundException
-     * @throws URISyntaxException
-     */
-    public static String validateMessage(File file) throws SAXException, MalformedURLException, FileNotFoundException, URISyntaxException{
-        StringBuilder errorOutput = new StringBuilder();
-        GenericSchemaValidator genericValidator = new GenericSchemaValidator();
-        genericValidator.setSchema(new URI(SCHEMA_LOCATION));
-        logger.info("XML schema validation on " + file.getName());
-        ErrorHandlerIface handler = new ValidationErrorHandler();
-        genericValidator.setErrorHandler(handler);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        genericValidator.validate(br);
-        List<String> errorMsgs = handler.getErrorMessages(); // ToDo: make ErrorHandlerIface type safe
-        for (String content : errorMsgs) {
-            errorOutput.append(content);
-            errorOutput.append(System.getProperty("line.separator"));
-        }
-        return errorOutput.toString();
+  /**
+   * Method to validate a supplied PX XML file.
+   *
+   * @param file the PX XML file to be validated.
+   * @return String which lists any errors that occurred during validation. If there were none, the String will be empty.
+   * @throws SAXException
+   * @throws MalformedURLException
+   * @throws FileNotFoundException
+   * @throws URISyntaxException
+   */
+  public static String validateMessage(File file) throws SAXException, MalformedURLException, FileNotFoundException, URISyntaxException{
+    StringBuilder errorOutput = new StringBuilder();
+    GenericSchemaValidator genericValidator = new GenericSchemaValidator();
+    genericValidator.setSchema(new URI(SCHEMA_LOCATION));
+    logger.info("XML schema validation on " + file.getName());
+    ErrorHandlerIface handler = new ValidationErrorHandler();
+    genericValidator.setErrorHandler(handler);
+    BufferedReader br = new BufferedReader(new FileReader(file));
+    genericValidator.validate(br);
+    List<String> errorMsgs = handler.getErrorMessages(); // ToDo: make ErrorHandlerIface type safe
+    for (String content : errorMsgs) {
+      errorOutput.append(content);
+      errorOutput.append(System.getProperty("line.separator"));
     }
+    return errorOutput.toString();
+  }
 }
