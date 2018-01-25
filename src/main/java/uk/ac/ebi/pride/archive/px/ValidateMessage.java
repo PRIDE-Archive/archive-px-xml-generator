859763fa61b3c759960dce0f1c10aadfdce0188d
Tobias-Ternent <tobias@ebi.ac.uk>
2018-01-25 08:16:28 +0000

    Added option for custom px schema location.

--- src/main/java/uk/ac/ebi/pride/archive/px/ValidateMessage.java
+++ src/main/java/uk/ac/ebi/pride/archive/px/ValidateMessage.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.pride.archive.px;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
@@ -25,7 +26,7 @@ public class ValidateMessage {
   private static final String SCHEMA_LOCATION = "http://ftp.pride.ebi.ac.uk/pride/resources/schema/proteomexchange/proteomeXchange-1.3.0.xsd";
 
   /**
-   * Method to validate a supplied PX XML file.
+   * Method to validate a supplied PX XML file using the default PX Schema location.
    *
    * @param file the PX XML file to be validated.
    * @return String which lists any errors that occurred during validation. If there were none, the String will be empty.
@@ -35,9 +36,24 @@ public class ValidateMessage {
    * @throws URISyntaxException
    */
   public static String validateMessage(File file) throws SAXException, MalformedURLException, FileNotFoundException, URISyntaxException{
+    return validateMessage(file, SCHEMA_LOCATION);
+  }
+
+  /**
+   * Method to validate a supplied PX XML file.
+   *
+   * @param file the PX XML file to be validated.
+   * @param schemaLocation the PX XML schema
+   * @return String which lists any errors that occurred during validation. If there were none, the String will be empty.
+   * @throws SAXException
+   * @throws MalformedURLException
+   * @throws FileNotFoundException
+   * @throws URISyntaxException
+   */
+  public static String validateMessage(File file, String schemaLocation) throws SAXException, MalformedURLException, FileNotFoundException, URISyntaxException{
     StringBuilder errorOutput = new StringBuilder();
     GenericSchemaValidator genericValidator = new GenericSchemaValidator();
-    genericValidator.setSchema(new URI(SCHEMA_LOCATION));
+    genericValidator.setSchema(new URI(StringUtils.isEmpty(schemaLocation) ? SCHEMA_LOCATION : schemaLocation));
     logger.info("XML schema validation on " + file.getName());
     ErrorHandlerIface handler = new ValidationErrorHandler();
     genericValidator.setErrorHandler(handler);
